b4013347950b9e0fdce182860edf4c85667030d8
Sergio Moya <1083296+smoya@users.noreply.github.com>
2021-07-12 19:21:56 +0200

    WIP

--- asyncapi/document.go
+++ asyncapi/document.go
@@ -4,14 +4,133 @@ package asyncapi
 // It's API implements https://github.com/asyncapi/parser-api/blob/master/docs/v1.md.
 type Document interface {
 	Extendable
+	Channels() []Channel
+	HasChannels() bool
+	ClientPublishableChannels() []Channel
+	ClientPublishableMessages() []Message
+	//ClientPublishOperations() []Operation
+	ClientSubscribableChannels() []Channel
+	ClientSubscribableMessages() []Message
+	//ClientSubscribeOperations() []Operation
+	Messages() []Message
 	Servers() []Server
 	HasServers() bool
+}
+
+// Channel is an addressable component, made available by the server, for the organization of messages.
+// Producer applications send messages to channels and consumer applications consume messages from channels
+type Channel interface {
+	Extendable
+	Identifiable
+	Path() string // Path is the identifier
+	Description() string
+	HasDescription() bool
+	Parameters() []ChannelParameter
+	HasParameters() bool
+	Operations() []Operation
+	Messages() []Message
+}
+
+// ChannelParameter describes a parameter included in a channel name.
+type ChannelParameter interface {
+	Extendable
+	Identifiable
+	Name() string
+	Description() string
+	Schema() Schema
+}
+
+// Operation describes a publish or a subscribe operation.
+// This provides a place to document how and why messages are sent and received.
+type Operation interface {
+	Extendable
+	Identifiable
+	Description() string
+	HasDescription() bool
+	IsApplicationPublishing() bool
+	IsApplicationSubscribing() bool
+	IsClientPublishing() bool
+	IsClientSubscribing() bool
+	Messages() []Message
+}
+
+// Message describes a message received on a given channel and operation.
+type Message interface {
+	Extendable
+	Identifiable
+	UID() string
+	Name() string
+	Title() string
+	HasTitle() bool
+	Description() string
+	HasDescription() bool
+	Summary() string
+	ContentType() string
+	Payload() Schema
+}
+
+// Schema is an object that allows the definition of input and output data types.
+// These types can be objects, but also primitives and arrays.
+// This object is a superset of the JSON Schema Specification Draft 07.
+type Schema interface {
+	Extendable
+	// $id() string
+	AdditionalItems() Schema
+	AdditionalProperties() Schema // (boolean | Schema)
+	AllOf() Schema
+	AnyOf() Schema
+	CircularProps() []string
+	Const() interface{}
+	Contains() Schema
+	ContentEncoding() string
+	ContentMediaType() string
+	Default() interface{}
+	Definitions() map[string]Schema
+	Dependencies() map[string]Schema // Map[string, Schema|string[]]
+	Deprecated() bool
+	Discriminator() string
+	Else() Schema
+	Enum() []interface{}
+	Examples() []interface{}
+	ExclusiveMaximum() float64
+	ExclusiveMinimum() float64
+	Format() string
+	HasCircularProps() bool
+	If() Schema
+	IsCircular() bool
+	Items() []Schema // Schema | Schema[]
+	Maximum() float64
+	MaxItems() float64
+	MaxLength() float64
+	MaxProperties() float64
+	Minimum() float64
+	MinItems() float64
+	MinLength() float64
+	MinProperties() float64
+	MultipleOf() float64
+	Not() Schema
+	OneOf() Schema
+	Pattern() string
+	PatternProperties() map[string]Schema
+	Properties() map[string]Schema
+	Property(name string) Schema
+	Name() string
+	PropertyNames() Schema
+	ReadOnly() bool
+	Required() string
+	Then() Schema
+	Title() string
+	Type() []string // string | string[]
+	Uid() string
+	UniqueItems() bool
+	WriteOnly() bool
 }
 
 // Server is an object representing a message broker, a server or any other kind of computer program capable of
 // sending and/or receiving data.
 type Server interface {
 	Extendable
+	Identifiable
 	Name() string
 	HasName() bool
 	Description() string
@@ -26,6 +145,7 @@ type Server interface {
 // ServerVariable is an object representing a Server Variable for server URL template substitution.
 type ServerVariable interface {
 	Extendable
+	Identifiable
 	Name() string
 	HasName() bool
 	DefaultValue() string
--- asyncapi/v2/v2.go
+++ asyncapi/v2/v2.go
@@ -7,6 +7,34 @@ type Document struct {
 	ServersField map[string]Server `mapstructure:"servers"`
 }
 
+func (d Document) Channels() []asyncapi.Channel {
+	panic("implement me")
+}
+
+func (d Document) HasChannels() bool {
+	panic("implement me")
+}
+
+func (d Document) ClientPublishableChannels() []asyncapi.Channel {
+	panic("implement me")
+}
+
+func (d Document) ClientPublishableMessages() []asyncapi.Message {
+	panic("implement me")
+}
+
+func (d Document) ClientSubscribableChannels() []asyncapi.Channel {
+	panic("implement me")
+}
+
+func (d Document) ClientSubscribableMessages() []asyncapi.Message {
+	panic("implement me")
+}
+
+func (d Document) Messages() []asyncapi.Message {
+	panic("implement me")
+}
+
 func (d Document) Servers() []asyncapi.Server {
 	var servers []asyncapi.Server
 	for _, s := range d.ServersField {
@@ -18,6 +46,395 @@ func (d Document) Servers() []asyncapi.Server {
 
 func (d Document) HasServers() bool {
 	return len(d.ServersField) > 0
+}
+
+type Channel struct {
+}
+
+func (c Channel) HasExtension(name string) bool {
+	panic("implement me")
+}
+
+func (c Channel) Extension(name string) interface{} {
+	panic("implement me")
+}
+
+func (c Channel) IDField() string {
+	panic("implement me")
+}
+
+func (c Channel) ID() string {
+	panic("implement me")
+}
+
+func (c Channel) Path() string {
+	panic("implement me")
+}
+
+func (c Channel) Description() string {
+	panic("implement me")
+}
+
+func (c Channel) HasDescription() bool {
+	panic("implement me")
+}
+
+func (c Channel) Parameters() []asyncapi.ChannelParameter {
+	panic("implement me")
+}
+
+func (c Channel) HasParameters() bool {
+	panic("implement me")
+}
+
+func (c Channel) Operations() []asyncapi.Operation {
+	panic("implement me")
+}
+
+func (c Channel) Messages() []asyncapi.Message {
+	panic("implement me")
+}
+
+type ChannelParameter struct {
+}
+
+func (c ChannelParameter) HasExtension(name string) bool {
+	panic("implement me")
+}
+
+func (c ChannelParameter) Extension(name string) interface{} {
+	panic("implement me")
+}
+
+func (c ChannelParameter) IDField() string {
+	panic("implement me")
+}
+
+func (c ChannelParameter) ID() string {
+	panic("implement me")
+}
+
+func (c ChannelParameter) Name() string {
+	panic("implement me")
+}
+
+func (c ChannelParameter) Description() string {
+	panic("implement me")
+}
+
+func (c ChannelParameter) Schema() asyncapi.Schema {
+	panic("implement me")
+}
+
+type Operation struct {
+}
+
+func (o Operation) HasExtension(name string) bool {
+	panic("implement me")
+}
+
+func (o Operation) Extension(name string) interface{} {
+	panic("implement me")
+}
+
+func (o Operation) IDField() string {
+	panic("implement me")
+}
+
+func (o Operation) ID() string {
+	panic("implement me")
+}
+
+func (o Operation) Description() string {
+	panic("implement me")
+}
+
+func (o Operation) HasDescription() bool {
+	panic("implement me")
+}
+
+func (o Operation) IsApplicationPublishing() bool {
+	panic("implement me")
+}
+
+func (o Operation) IsApplicationSubscribing() bool {
+	panic("implement me")
+}
+
+func (o Operation) IsClientPublishing() bool {
+	panic("implement me")
+}
+
+func (o Operation) IsClientSubscribing() bool {
+	panic("implement me")
+}
+
+func (o Operation) Messages() []asyncapi.Message {
+	panic("implement me")
+}
+
+type Message struct {
+	Extendable
+	asyncapi.Identifiable
+}
+
+func (m Message) HasExtension(name string) bool {
+	panic("implement me")
+}
+
+func (m Message) Extension(name string) interface{} {
+	panic("implement me")
+}
+
+func (m Message) IDField() string {
+	panic("implement me")
+}
+
+func (m Message) ID() string {
+	panic("implement me")
+}
+
+func (m Message) UID() string {
+	panic("implement me")
+}
+
+func (m Message) Name() string {
+	panic("implement me")
+}
+
+func (m Message) Title() string {
+	panic("implement me")
+}
+
+func (m Message) HasTitle() bool {
+	panic("implement me")
+}
+
+func (m Message) Description() string {
+	panic("implement me")
+}
+
+func (m Message) HasDescription() bool {
+	panic("implement me")
+}
+
+func (m Message) Summary() string {
+	panic("implement me")
+}
+
+func (m Message) ContentType() string {
+	panic("implement me")
+}
+
+func (m Message) Payload() asyncapi.Schema {
+	panic("implement me")
+}
+
+type Schema struct {
+}
+
+func (s Schema) HasExtension(name string) bool {
+	panic("implement me")
+}
+
+func (s Schema) Extension(name string) interface{} {
+	panic("implement me")
+}
+
+func (s Schema) AdditionalItems() asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) AdditionalProperties() asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) AllOf() asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) AnyOf() asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) CircularProps() []string {
+	panic("implement me")
+}
+
+func (s Schema) Const() interface{} {
+	panic("implement me")
+}
+
+func (s Schema) Contains() asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) ContentEncoding() string {
+	panic("implement me")
+}
+
+func (s Schema) ContentMediaType() string {
+	panic("implement me")
+}
+
+func (s Schema) Default() interface{} {
+	panic("implement me")
+}
+
+func (s Schema) Definitions() map[string]asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) Dependencies() map[string]asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) Deprecated() bool {
+	panic("implement me")
+}
+
+func (s Schema) Discriminator() string {
+	panic("implement me")
+}
+
+func (s Schema) Else() asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) Enum() []interface{} {
+	panic("implement me")
+}
+
+func (s Schema) Examples() []interface{} {
+	panic("implement me")
+}
+
+func (s Schema) ExclusiveMaximum() float64 {
+	panic("implement me")
+}
+
+func (s Schema) ExclusiveMinimum() float64 {
+	panic("implement me")
+}
+
+func (s Schema) Format() string {
+	panic("implement me")
+}
+
+func (s Schema) HasCircularProps() bool {
+	panic("implement me")
+}
+
+func (s Schema) If() asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) IsCircular() bool {
+	panic("implement me")
+}
+
+func (s Schema) Items() []asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) Maximum() float64 {
+	panic("implement me")
+}
+
+func (s Schema) MaxItems() float64 {
+	panic("implement me")
+}
+
+func (s Schema) MaxLength() float64 {
+	panic("implement me")
+}
+
+func (s Schema) MaxProperties() float64 {
+	panic("implement me")
+}
+
+func (s Schema) Minimum() float64 {
+	panic("implement me")
+}
+
+func (s Schema) MinItems() float64 {
+	panic("implement me")
+}
+
+func (s Schema) MinLength() float64 {
+	panic("implement me")
+}
+
+func (s Schema) MinProperties() float64 {
+	panic("implement me")
+}
+
+func (s Schema) MultipleOf() float64 {
+	panic("implement me")
+}
+
+func (s Schema) Not() asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) OneOf() asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) Pattern() string {
+	panic("implement me")
+}
+
+func (s Schema) PatternProperties() map[string]asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) Properties() map[string]asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) Property(name string) asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) Name() string {
+	panic("implement me")
+}
+
+func (s Schema) PropertyNames() asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) ReadOnly() bool {
+	panic("implement me")
+}
+
+func (s Schema) Required() string {
+	panic("implement me")
+}
+
+func (s Schema) Then() asyncapi.Schema {
+	panic("implement me")
+}
+
+func (s Schema) Title() string {
+	panic("implement me")
+}
+
+func (s Schema) Type() []string {
+	panic("implement me")
+}
+
+func (s Schema) Uid() string {
+	panic("implement me")
+}
+
+func (s Schema) UniqueItems() bool {
+	panic("implement me")
+}
+
+func (s Schema) WriteOnly() bool {
+	panic("implement me")
 }
 
 type Server struct {
